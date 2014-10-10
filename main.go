c20ec9e98448e9b448aad770a8912548f871b1c0
Jason Mooberry <jasonmoo@me.com>
2014-10-10 13:44:04 -0400

    adding flags n stuff

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go/ast"
 	"go/parser"
@@ -15,112 +16,108 @@ import (
 
 type (
 	Dog struct {
-		Name   string `json:"name"bson:"nome"`
+		Name   string `json:"name"bson:"nome,omitempty"`
 		Height string `json:"hieght"bson:"height"`
 	}
 )
 
 var (
-	quotedValueRegexp = regexp.MustCompile(`([^:]+):"([^"]+?)(?:,omitempty)?"`)
+	anagram        = flag.Bool("anagram", false, "check for anagrams (commonly misspellings)")
+	un_camel_case  = flag.Bool("un_camel_case", false, "match field names to tag names un-camelcased.  Ex: WalterWhite -> walter_white")
+	all_tags_match = flag.Bool("all_tags_match", false, "ensure all tags match")
+	levenshtein    = flag.Int("levenshtein", 0, "report if levenshtein distance > n")
+
+	quotedValueRegexp = regexp.MustCompile(`([^:]+):"([^",]+)[^"]*"`)
 	unCamelCaseRegexp = regexp.MustCompile(`([A-Z]+)`)
 )
 
+func init() {
+	flag.Parse()
+	if flag.NFlag() == 0 || flag.NArg() == 0 {
+		fmt.Println("taggert usage:")
+		fmt.Println("./taggert [-flags] source.go")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
+
 func main() {
 
-	fset := token.NewFileSet() // positions are relative to fset
 	w := &tabwriter.Writer{}
-	w.Init(os.Stdout, 0, 12, 0, '\t', 0)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
 	defer w.Flush()
 
-	// Parse the file containing this very example
-	// but stop after processing the imports.
-	f, err := parser.ParseFile(fset, os.Args[1], nil, 0)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	fset := token.NewFileSet()
 
-	// Print the imports from the file's AST.
-	for _, s := range f.Decls {
+	for _, file := range flag.Args() {
 
-		node, ok := s.(*ast.GenDecl)
-		if !ok || node.Tok != token.TYPE {
-			continue
+		f, err := parser.ParseFile(fset, file, nil, 0)
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 
-		for _, spec := range node.Specs {
-			ts, ok := spec.(*ast.TypeSpec)
-			if !ok {
+		for _, s := range f.Decls {
+
+			node, ok := s.(*ast.GenDecl)
+			if !ok || node.Tok != token.TYPE {
 				continue
 			}
 
-			stct, ok := ts.Type.(*ast.StructType)
-			if !ok {
-				continue
-			}
-
-			for _, field := range stct.Fields.List {
-
-				if len(field.Names) != 1 {
+			for _, spec := range node.Specs {
+				ts, ok := spec.(*ast.TypeSpec)
+				if !ok {
 					continue
 				}
 
-				if field.Tag != nil {
-					us_name := ccToUS(field.Names[0].String())
-					matches := quotedValueRegexp.FindAllStringSubmatch(field.Tag.Value[1:], -1)
-					for _, match := range matches {
-						if len(match) != 3 {
-							continue
-						}
-						if msg := Test(us_name, match[2]); msg != "" {
-							fmt.Fprintf(
-								w,
-								"%s:%d\tField name %s\t\t%s\t%s:%q\n",
-								os.Args[1],
-								fset.File(field.Pos()).Line(field.Pos()),
-								msg,
-								field.Names[0].String(),
-								match[1],
-								match[2],
-							)
-						}
+				stct, ok := ts.Type.(*ast.StructType)
+				if !ok {
+					continue
+				}
+
+				for _, field := range stct.Fields.List {
+
+					if len(field.Names) != 1 {
+						continue
 					}
-					if len(matches) > 1 {
-						for i := 1; i < len(matches); i++ {
-							if msg := Test(matches[0][1], matches[i][1]); msg != "" {
+
+					if field.Tag != nil {
+						matches := quotedValueRegexp.FindAllStringSubmatch(field.Tag.Value[1:], -1)
+						for _, match := range matches {
+							if len(match) != 3 {
+								continue
+							}
+							if msg := Test(field.Names[0].String(), match[2]); msg != "" {
 								fmt.Fprintf(
 									w,
-									"%s:%d\tTag name incongruency\t%s\t%s:%q\t%s:%q\n",
-									os.Args[1],
+									"%s:%d\tField name %s\t%q\t%s:%q\n",
+									file,
 									fset.File(field.Pos()).Line(field.Pos()),
 									msg,
-									matches[0][1],
-									matches[0][2],
-									matches[i][1],
-									matches[i][2],
+									field.Names[0].String(),
+									match[1],
+									match[2],
 								)
 							}
 						}
-
+						if *all_tags_match && len(matches) > 1 {
+							for i := 1; i < len(matches); i++ {
+								if msg := Test(matches[0][1], matches[i][1]); msg != "" {
+									fmt.Fprintf(
+										w,
+										"%s:%d\tTag name %s\t%s:%q\t%s:%q\n",
+										file,
+										fset.File(field.Pos()).Line(field.Pos()),
+										msg,
+										matches[0][1],
+										matches[0][2],
+										matches[i][1],
+										matches[i][2],
+									)
+								}
+							}
+						}
 					}
-
-					// if len(matches) == 2 {
-					// 	if msg := Test(matches[0][2], matches[1][2], field.Names[0].String()); msg != "" {
-					// 		fmt.Printf(
-					// 			"%s:%d  %s (%s): %s:%q : %s:%q\n",
-					// 			os.Args[1],
-					// 			fset.File(field.Pos()).Line(field.Pos()),
-					// 			msg,
-					// 			field.Names[0].String(),
-					// 			// uc_name,
-					// 			// match[1],
-					// 			matches[0][1],
-					// 			matches[0][2],
-					// 			matches[1][1],
-					// 			matches[1][2],
-					// 		)
-					// 	}
-					// }
 				}
 			}
 		}
@@ -134,8 +131,10 @@ func ccToUS(s string) string {
 }
 
 func Test(a, b string) string {
-	a, b = ccToUS(a), ccToUS(b)
-	if IsAnagram(a, b) {
+	if *un_camel_case {
+		a, b = ccToUS(a), ccToUS(b)
+	}
+	if *anagram && IsAnagram(a, b) {
 		return "anagram"
 	}
 	if a != b {
@@ -145,7 +144,8 @@ func Test(a, b string) string {
 }
 
 func IsAnagram(a, b string) bool {
-	if a == b || len(a) != len(b) {
+	a, b = strings.ToLower(a), strings.ToLower(b)
+	if len(a) != len(b) || a == b {
 		return false
 	}
 	ai := make([]int, utf8.RuneCountInString(a))
